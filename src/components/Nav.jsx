0593cb0e95fcfb856a7411f2098c31ed9ffe3908
Colin Whitney <colin.whitney@gmail.com>
2020-12-11 12:10:34 -0500

    cleaned up navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,8 +4,14 @@ import Nav from 'react-bootstrap/Nav'
 
 export default function Navigation() {
   return (
-    <Navbar bg="primary" variant="dark">
+    <Navbar bg="primary" >
       <Navbar.Brand href="/">Village Kitchen</Navbar.Brand>
+      <Navbar.Collapse className="justify-content-end">
+        <Nav className="flex-row">
+          <Nav.Link href="/login">Log In</Nav.Link>
+          <Nav.Link href="/register">Sign Up</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   )
 }
