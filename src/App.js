a5cf66ad3fa66659cc4f2ef505da9947172bade1
Colin Whitney <colin.whitney@gmail.com>
2020-12-11 10:51:49 -0500

    started building app; added bootstrap and router-dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom';
+// import { Container, Button } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container'
 import { useSelector, useDispatch } from 'react-redux';
 import {increment, decrement} from './actions/index';
-import Post from './components/Post';
-import Postform from './components/Postform';
+import Home from './components/Home';
+
 
 export default function App() {
   const counter = useSelector(state => state.counter);
@@ -10,12 +14,15 @@ export default function App() {
   const dispatch = useDispatch();
 
   return (
-    <div>
-      <h1>Counter {counter}</h1>
-      <button onClick={() => dispatch(increment(5))}>+</button>
-      <button onClick={() => dispatch(decrement())}>-</button>
-      
-    </div>
+    <Router>
+      <Container>
+        <h1>Village Kitchen</h1>
+        <Button variant="primary">Primary</Button>{' '}
+        <Switch>
+          <Route path="/" exact component={Home}/>
+        </Switch>
+      </Container>
+    </Router>
   )
 }
 
---
+++ src/components/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Find a home cooked meal near you!</h1>
+    </div>
+  )
+}
