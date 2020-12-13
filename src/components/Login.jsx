7c463c8e01eeeb4d9f7a07a6f634b234374ab55b
Colin Whitney <colin.whitney@gmail.com>
2020-12-13 17:43:02 -0500

    added hooks to the login page; updating state on change

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 
 export default function Login() {
+  const [email, setEmail] = useState()
+  const [password, setPassword] = useState()
   return (
     <Form>
       <Form.Group controlId="email">
         <Form.Label>Email</Form.Label>
-        <Form.Control type="email" />
+        <Form.Control type="email" name="email" value={email} onChange={e => setEmail(e.target.value)}/>
       </Form.Group>
       <Form.Group controlId="password">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" />
+        <Form.Control type="password" name="password" value={password} onChange={e => setPassword(e.target.value)}/>
       </Form.Group>
       <Button style={{backgroundColor: "salmon"}}>Submit</Button>
     </Form>
