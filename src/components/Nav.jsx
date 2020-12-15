f7777755fa7c10fd5828f2d367c12f18e0fc0b79
Colin Whitney <colin.whitney@gmail.com>
2020-12-15 11:22:52 -0500

    added button to Navbar for Logout route

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
+import Button from 'react-bootstrap/Button'
 import chef from '../chef.svg'
 
 export default function Navigation() {
@@ -17,6 +18,7 @@ export default function Navigation() {
         Village Kitchen</Navbar.Brand>
       <Navbar.Collapse className="justify-content-end">
         <Nav className="flex-row">
+          <Button type="submit" variant="outline-dark">Log Out</Button>
           <Nav.Link href="/login">Log In</Nav.Link>
           <Nav.Link href="/register">Sign Up</Nav.Link>
         </Nav>
